9965c494aa3128e7b8f661fdf621e8c94262e668
Sam-Nyalik <samnyalik@gmail.com>
2022-10-21 12:59:31 -0700

    Update

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,16 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
-#include "main.h"
 
-int main (void)
-{
-	int a,b,c;
-	int largest;
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
 
-	a = 972;
-	b = -98;
-	c = 0;
-
-	largest = largest_number(a,b,c);
-	printf("%d is the largest number\n", largest);
-	return (0);
-}
+#endif
